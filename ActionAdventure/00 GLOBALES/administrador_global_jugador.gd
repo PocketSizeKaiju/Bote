823b54092bf9b5f343711819d4fd14fe8a1d8983
PocketSizeKaiju <157993298+PocketSizeKaiju@users.noreply.github.com>
2024-12-01 12:28:34 -0300

    .

--- ActionAdventure/00 GLOBALES/administrador_global_jugador.gd
+++ ActionAdventure/00 GLOBALES/administrador_global_jugador.gd
@@ -7,15 +7,20 @@ var jugador_invocado: bool = false
 
 func _ready() -> void:
 	if get_tree().get_current_scene().name == "playground":
-		agregar_instancia_jugador()
-		await get_tree().create_timer(0.2).timeout
-		jugador_invocado = true
+		invocar_jugador()
+
+func invocar_jugador():
+	agregar_instancia_jugador()
+	await get_tree().create_timer(0.2).timeout
+	jugador_invocado = true
 
 func agregar_instancia_jugador() -> void:
 	jugador = JUGADOR.instantiate()
 	add_child(jugador)
 
 func asignar_posicion_jugador(_nueva_pos: Vector2) -> void:
+	if !jugador:
+		invocar_jugador()
 	jugador.global_position = _nueva_pos
 
 func asignar_como_padre(_padre: Node2D) -> void:
---
+++ bote_malo_area.gd
@@ -0,0 +1,5 @@
+extends Area3D
+
+func _cuando_entra_un_cuerpo(_body_rid: RID, _body: Node, _body_shape_index: int, _local_shape_index: int) -> void:
+	if _body.name == "Bote":
+		get_tree().change_scene_to_file("res://ActionAdventure/Niveles/Dentro de botes/01.tscn")
